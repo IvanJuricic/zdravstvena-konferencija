2ca45048815599cc07e0516f1e46858cee17aa2c
IvanJuricic <ivanjuricic97@gmail.com>
2020-10-27 12:45:07 +0100

    Modify models

--- models/Profile.js
+++ models/Profile.js
@@ -1,114 +1,33 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const profileSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user"
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  paper: {
+    type: String,
+  },
+  social: {
+    youtube: {
+      type: String,
     },
-    company: {
-        type: String
+    twitter: {
+      type: String,
     },
-    website: {
-        type: String
+    facebook: {
+      type: String,
     },
-    location: {
-        type: String
+    linkedin: {
+      type: String,
     },
-    status: {
-        type: String,
-        required: true
+    instagram: {
+      type: String,
     },
-    skills: {
-        type: [String],
-        required: true
-    },
-    bio: {
-        type: String
-    },
-    githubusername: {
-        type: String
-    },
-    experience: [
-        {
-            title: {
-                type:String,
-                required: true
-            },
-            company: {
-                type: String,
-                required: true
-            },
-            location: {
-                type: String
-            },
-            from: {
-                type: Date,
-                required: true
-            },
-            to: {
-                type: Date
-            },
-            current: {
-                type: Boolean,
-                default: false
-            },
-            description: {
-                type: String
-            }
-        }
-    ],
-    education: [
-        {
-            school: {
-                type: String,
-                required: true
-            },
-            degree: {
-                type: String,
-                required: true
-            },
-            fieldofstudy: {
-                type: String,
-                required: true
-            },
-            from: {
-                type: Date,
-                required: true
-            },
-            to: {
-                type: Date
-            },
-            current: {
-                type: Boolean,
-                default: false
-            },
-            description: {
-                type: String
-            }
-
-        }
-    ],
-    social: {
-        youtube: {
-            type: String
-        },
-        twitter: {
-            type: String
-        },
-        facebook: {
-            type: String
-        },
-        linkedin: {
-            type: String
-        },
-        instagram: {
-            type: String
-        }
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
-
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = Profile = mongoose.model("profile", profileSchema)
+module.exports = Profile = mongoose.model("profile", profileSchema);
--- models/User.js
+++ models/User.js
@@ -1,26 +1,40 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    avatar: {
-        type: String
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  avatar: {
+    type: String,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  authors: {
+    type: String,
+    required: true,
+  },
+  section: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+  },
+  id: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = User = mongoose.model("user", UserSchema);
